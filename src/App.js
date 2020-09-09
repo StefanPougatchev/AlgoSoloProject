0acf4e522f65103f71271d5b911f2ebad6c94805
“StefanPougatchev” <Stefan.Pougatchev@gmail.com>
2020-09-09 14:50:15 -0700

    merge sort completed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AlgoPresent from "./AlgoPresentation/AlgoPresentation.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <AlgoPresent></AlgoPresent>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,4 @@ import ReactDOM from "react-dom";
 import "./styles.scss";
 import App from "./App";
 
-const Index = () => {
-  return <div>Welcome to Sorting Bars</div>;
-};
-
 ReactDOM.render(<App />, document.getElementById("root"));
