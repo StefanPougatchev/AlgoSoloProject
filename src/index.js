b72cd27671bd744f0bf5f4755b6251df7b1b2ddf
“StefanPougatchev” <Stefan.Pougatchev@gmail.com>
2020-09-09 12:03:46 -0700

    boiler plate of proejct

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.scss";
+import App from "./App";
+
+const Index = () => {
+  return <div>Welcome to Sorting Bars</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HTMLWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const htmlPlugin = new HTMLWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html",
+});
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [htmlPlugin],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: "file-loader",
+        options: { name: "/static/[name].[ext]" },
+      },
+    ],
+  },
+};
